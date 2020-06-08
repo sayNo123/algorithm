6e903987fd7401cc6761c38c4e7567f48220b49c
xinuo <vax_sayno@163.com>
2020-06-08 18:56:11 +0800

    xinuo

---
+++ 两数之和/twosum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function (nums, target) {
+  const diffs = {};
+  // 缓存数组长度
+  const len = nums.length;
+  // 遍历数组
+  for (let i = 0; i < len; i++) {
+    // 判断当前值对应的 target 差值是否存在
+    if (diffs[target - nums[i]] !== undefined) {
+      // 若有对应差值，那么得到答案
+      return [diffs[target - nums[i]], i];
+    }
+    // 若没有对应差值，则记录当前值
+    diffs[nums[i]] = i;
+  }
+};
+var numArr = [11, 15, 2, 7]
+console.log(twoSum(numArr, 9))
